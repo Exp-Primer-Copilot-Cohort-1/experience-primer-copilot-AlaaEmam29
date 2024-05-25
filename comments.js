444e3bea9316c474dce2edc967801616b9e8a330
alaa.shaban.527 <alaa.shaban.527@gmail.com>
2024-05-25 19:11:09 +0300

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,5 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file.
+// The comments.html file should be served with the following content type: text/html.
+// The comments.html file should be located in the public directory.
+// The comments.html file should contain the following HTML:
